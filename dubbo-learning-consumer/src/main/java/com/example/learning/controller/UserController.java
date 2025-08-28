8f12d0b69079a253b77e081c977ce64649c20159
chenzhiwei <hai.pan@live.com>
2025-08-28 18:31:44 +0800

    修改url路径

--- dubbo-learning-consumer/src/main/java/com/example/learning/controller/UserController.java
+++ dubbo-learning-consumer/src/main/java/com/example/learning/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("order")
+@RequestMapping("user")
 public class UserController {
     @Autowired
     @DubboReference
