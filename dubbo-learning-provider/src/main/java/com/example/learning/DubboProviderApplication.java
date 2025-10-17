2ee3088f6e6134b5f32f1b9a11931f5e979471f1
chenzhiwei <hai.pan@live.com>
2025-10-17 13:31:09 +0800

    Spring启动过程分析

--- dubbo-learning-provider/src/main/java/com/example/learning/DubboProviderApplication.java
+++ dubbo-learning-provider/src/main/java/com/example/learning/DubboProviderApplication.java
@@ -1,7 +1,11 @@
 package com.example.learning;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ArrayListMultimap;
 import org.apache.dubbo.config.spring.beans.factory.annotation.ServiceAnnotationPostProcessor;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +19,14 @@ public class DubboProviderApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DubboProviderApplication.class, args);
         ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+        ArrayListMultimap arrayListMultimap = ArrayListMultimap.create();
+        for (String beanDefinitionName : beanDefinitionNames){
+            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+            String name = beanDefinition.getClass().getName();
+            arrayListMultimap.put(name, MoreObjects.firstNonNull(beanDefinition.getBeanClassName(),"bean名称："+beanDefinitionName));
+        }
+        System.out.println(JSON.toJSONString(arrayListMultimap));
         System.out.println("DubboProviderApplication已启动");
     }
 }
